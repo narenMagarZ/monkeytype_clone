1690e30f1499ce7c2f70eda4af51662ff5d9cd81
narenMagarZ <narenmagarz98@gmail.com>
2022-08-06 11:57:12 +0545

    component to show the result (wpm)

--- src/component/result/result.js
+++ src/component/result/result.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from 'react'
+import {useSelector} from 'react-redux'
+
 import '../../style/result.css'
 function Result(){
+    const {wordTyped,errorChar} = useSelector((state)=>state.WORDSTYPED)
+    const isTimerStopped = useSelector((state)=>state.STARTTIMER)
+    const [wpm,setWpm] = useState(0)
+    useEffect(()=>{
+        console.log(isTimerStopped)
+        if(isTimerStopped){
+            setWpm(()=>calculateWpm(wordTyped))
+        }
+    },[wordTyped,errorChar,isTimerStopped])
+    const calculateWpm = (wordsTyped)=>{
+        const wpm = Math.floor(wordsTyped / (1))
+        return wpm
+    }
     return(
-        <div>
-
+        <div className='result-container'>
+            <span id='wpm-value'>
+                {wpm}
+            </span>
         </div>
     )
 }
