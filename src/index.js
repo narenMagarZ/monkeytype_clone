76c081148257799d25903578a8816b822fd5921a
narenMagarZ <narenmagarz98@gmail.com>
2022-08-06 11:23:41 +0545

    configure to add redux store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css'
 import App from './app';
-
+import {Provider} from 'react-redux'
+import{ store } from './store'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
